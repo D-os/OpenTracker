9415754734ae7d424233f9009e6eff9c5f709a14
zooey <>
2004-01-02 17:43:46 +0000

    - applied some changes for better compliance with OT-StyleGuide   (I am trying hard >:o)

--- locale/include/Catalog.h
+++ locale/include/Catalog.h
@@ -16,13 +16,14 @@ class BCatalog {
 		BCatalog(const char *signature, const char *language = NULL);
 		~BCatalog();
 
-		const char *GetString(const char *string,
-									 const char *context=NULL,
-									 const char *comment=NULL);
+		const char *GetString(const char *string, const char *context=NULL,
+						const char *comment=NULL);
 		const char *GetString(uint32 id);
 
 		status_t GetData(const char *name, BMessage *msg);
 		status_t GetData(uint32 id, BMessage *msg);
+
+		status_t InitCheck() const;
 
 	private:
 		BCatalogAddOn *fCatalog;
@@ -37,23 +38,24 @@ class BCatalog {
 class BCatalogAddOn {
 		friend class BLocaleRoster;
 	public:
-		BCatalogAddOn(const char *signature, 
-						  const char *language,
-						  BCatalogAddOnInfo* info);
+		BCatalogAddOn(const char *signature, const char *language,
+			BCatalogAddOnInfo* info);
 		virtual ~BCatalogAddOn();
 
-		virtual const char *GetString(const char *string,
-												const char *context=NULL,
-												const char *comment=NULL) = 0;
+		virtual const char *GetString(const char *string, 
+								const char *context=NULL,
+								const char *comment=NULL) = 0;
 		virtual const char *GetString(uint32 id) = 0;
 
 		// ToDo: setting the translated strings is not yet implemented:
 		virtual bool CanWriteData() const;
 		virtual status_t SetString(const char *string, 
-											const char *translated,
-											const char *context=NULL,
-											const char *comment=NULL);
+							const char *translated,
+							const char *context=NULL,
+							const char *comment=NULL);
 		virtual status_t SetString(int32 id, const char *translated);
+
+		status_t InitCheck() const;
 
 		// the following could be used to localize non-textual data (e.g. icons),
 		// but these will only be implemented if there's demand for such a 
@@ -65,15 +67,24 @@ class BCatalogAddOn {
 		virtual status_t SetData(uint32 id, BMessage *msg);
 
 	protected:
-		BString fSignature;
-		BCatalogAddOnInfo *fAddOnInfo;
-		BString fLanguageName;
-		BCatalogAddOn *fNext;
+		BString 			fSignature;
+		BCatalogAddOnInfo 	*fAddOnInfo;
+		BString 			fLanguageName;
+		BCatalogAddOn 		*fNext;
+		status_t 			fInitCheck;
 };
 
-// every catalog-add-on should export the followin function:
-extern "C" BCatalogAddOn *instantiate_catalog( const char* signature,
-															  const char* language,
-															  BCatalogAddOnInfo* info);
+inline status_t 
+BCatalogAddOn::InitCheck() const
+{ 
+	return fInitCheck;
+}
+
+// every catalog-add-on should export two symbols...
+// ...the function that instantiates a catalog for this add-on-type...
+extern "C" BCatalogAddOn *instantiate_catalog(const char* signature,
+							const char* language, BCatalogAddOnInfo* info);
+// ...and the priority which will be used to order the catalog-add-ons:
+extern uint8 gCatalogAddOnPriority;
 
 #endif	/* _CATALOG_H_ */
