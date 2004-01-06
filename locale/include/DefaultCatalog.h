6ceb5e0ea70743655dc464129047f26c91017f89
zooey <>
2004-01-06 18:25:09 +0000

    - some more groundwork done, basic loading/storing methods exist. - hashmap-loading still pending.

--- locale/include/DefaultCatalog.h
+++ locale/include/DefaultCatalog.h
@@ -1,32 +1,61 @@
 #ifndef _DEFAULT_CATALOG_H_
 #define _DEFAULT_CATALOG_H_
 
+#include <hash_map>
+
 #include <Catalog.h>
 #include <String.h>
 
-class BCatalogAddOn;
-class BCatalogAddOnInfo;
-class BLocale;
-class BMessage;
+namespace BPrivate {
 
+struct CatKey {
+	BString fKey;
+		// the key consists of three values separated by a special token:
+		// - the native string
+		// - the context of the string's usage
+		// - a comment that can be used to separate strings that
+		//   are identical otherwise (useful for the translator)
+	size_t fHashVal;
+		// the hash-value of fKey
+	CatKey(const char *str, const char *ctx, const char *cmt);
+};
+
+__STL_TEMPLATE_NULL struct hash<CatKey>
+{
+  size_t operator()(const BPrivate::CatKey &key) const;
+};
 
 class DefaultCatalog : public BCatalogAddOn {
 
 	public:
+		DefaultCatalog(const char *signature, const char *language);
 		DefaultCatalog(const char *signature, const char *language,
-			BCatalogAddOnInfo* info);
+			const char *path, bool create = false);
 		~DefaultCatalog();
 
 		const char *GetString(const char *string, const char *context=NULL,
-							const char *comment=NULL);
+						const char *comment=NULL);
 		const char *GetString(uint32 id);
 
-		static BCatalogAddOn* Instantiate(const char *signature,
-										const char *language, BCatalogAddOnInfo *info);
+		status_t SetString(const char *string, const char *translated, 
+					const char *context=NULL, const char *comment=NULL);
+		status_t SetString(uint32 id, const char *translated);
+
+		status_t LoadFromDisk(const char *path);
+		status_t WriteToDisk(const char *path = NULL) const;
+
+		static BCatalogAddOn *Instantiate(const char *signature,
+								const char *language);
 		static const uint8 DefaultCatalog::gDefaultCatalogAddOnPriority;
 
 	private:
-
+		typedef hash_map<CatKey, BString> CatMap;
+		CatMap fCatMap;
+		mutable BString fPath;
 };
+
+}	// namespace BPrivate
+
+using namespace BPrivate;
 
 #endif	/* _DEFAULT_CATALOG_H_ */
--- locale/lib/DefaultCatalog.cpp
+++ locale/lib/DefaultCatalog.cpp
@@ -15,60 +15,146 @@
 #include <DefaultCatalog.h>
 #include <LocaleRoster.h>
 
-/**	This implements the default catalog-type for the opentracker locale kit
- *    Alternatively, this could be used as a full add-on, but currently this
- *    is provided as part of liblocale.so.
+/*
+ *	This implements the default catalog-type for the opentracker locale kit.
+ *  Alternatively, this could be used as a full add-on, but currently this
+ *  is provided as part of liblocale.so.
  */
-
-DefaultCatalog::DefaultCatalog(const char *signature, const char *language,
-	BCatalogAddOnInfo* info)
-	:	
-	BCatalogAddOn(signature, language, info)
+size_t hash<CatKey>::operator()(const CatKey &key) const 
 {
+	return key.fHashVal;
+}
+
+static const char kSeparator = '\01';
+
+CatKey::CatKey(const char *str, const char *ctx, const char *cmt)
+{
+	uint32 strLen = str ? strlen(str) : 0;
+	uint32 ctxLen = ctx ? strlen(ctx) : 0;
+	uint32 cmtLen = cmt ? strlen(cmt) : 0;
+	int32 keyLen = strLen + ctxLen + cmtLen + 2;
+	char *keyBuf = fKey.LockBuffer(keyLen);
+	if (!keyBuf)
+		return;
+	if (strLen) {
+		memcpy(keyBuf, str, strLen);
+		keyBuf += strLen;
+	}
+	*keyBuf++ = kSeparator;
+	if (ctxLen) {
+		memcpy(keyBuf, ctx, ctxLen);
+		keyBuf += ctxLen;
+	}
+	*keyBuf++ = kSeparator;
+	if (cmtLen) {
+		memcpy(keyBuf, cmt, cmtLen);
+		keyBuf += cmtLen;
+	}
+	fKey.UnlockBuffer(keyLen);
+}
+
+DefaultCatalog::DefaultCatalog(const char *signature, const char *language)
+	:
+	BCatalogAddOn(signature, language)
+{
+	static const char *catFolder = "catalogs";
+	static const char *catExtension = ".catalog";
 	app_info appInfo;
 	be_app->GetAppInfo(&appInfo); 
 	node_ref nref;
 	nref.device = appInfo.ref.device;
 	nref.node = appInfo.ref.directory;
 	BDirectory appDir(&nref);
-	BString catalogName("catalogs/");
-	catalogName << signature << "/" << language << ".catalog";
+	BString catalogName(catFolder);
+	catalogName << "/" << fLanguageName << "/" << fSignature << catExtension;
 	BPath catalogPath(&appDir, catalogName.String());
-	BFile catalogFile;
-	catalogFile.SetTo(catalogPath.Path(), B_READ_ONLY);
-	if (catalogFile.InitCheck() != B_OK) {
+	status_t status = LoadFromDisk( catalogPath.Path());
+
+	if (status != B_OK) {
 		BPath commonEtcPath;
 		find_directory(B_COMMON_ETC_DIRECTORY, &commonEtcPath);
 		if (commonEtcPath.InitCheck() == B_OK) {
 			catalogName = BString(commonEtcPath.Path()) 
-								<< "/locale/catalogs/" 
-								<< signature << "/" << language << ".catalog";
-			catalogFile.SetTo(catalogName.String(), B_READ_ONLY);
+							<< "/locale/" << catFolder 
+							<< "/" << fLanguageName 
+							<< "/" << fSignature << catExtension;
+			status = LoadFromDisk(catalogName.String());
 		}
 	}
-	if (catalogFile.InitCheck() != B_OK) {
+
+	if (status != B_OK) {
 		BPath systemEtcPath;
 		find_directory(B_BEOS_ETC_DIRECTORY, &systemEtcPath);
 		if (systemEtcPath.InitCheck() == B_OK) {
-			catalogName = BString(systemEtcPath.Path()) << "/locale/catalogs/" 
-				<< signature << "/" << language;
-			catalogFile.SetTo(catalogName.String(), B_READ_ONLY);
+			catalogName = BString(systemEtcPath.Path()) 
+							<< "/locale/" << catFolder 
+							<< "/" << fLanguageName
+							<< "/" << fSignature << catExtension;
+			status = LoadFromDisk(catalogName.String());
 		}
 	}
-	if (catalogFile.InitCheck() != B_OK) {
-		fInitCheck = B_NAME_NOT_FOUND;
-		return;
-	}
 
-	BMessage catalogMsg;
-	fInitCheck = catalogMsg.Unflatten(&catalogFile);
-	if (fInitCheck == B_OK) {
-		// ToDo: create hash-map from catalogMsg
-	}
+	fInitCheck = status;
+}
+
+DefaultCatalog::DefaultCatalog(const char *signature, const char *language,
+	const char *path, bool create)
+	:
+	BCatalogAddOn(signature, language),
+	fPath(path)
+{
+	fInitCheck = LoadFromDisk(path);
+	if (fInitCheck != B_OK && create && path)
+		// catalog not found, but we shall create it, so we say we're ok:
+		fInitCheck = B_OK;
 }
 
 DefaultCatalog::~DefaultCatalog()
 {
+}
+
+status_t
+DefaultCatalog::LoadFromDisk(const char *path)
+{
+	if (!path)
+		return B_BAD_VALUE;
+
+	BFile catalogFile;
+	status_t res = catalogFile.SetTo(path, B_READ_ONLY);
+	if (res != B_OK)
+		return res;
+
+	fPath = path;
+	BMessage catalogMsg;
+	res = catalogMsg.Unflatten(&catalogFile);
+	if (res != B_OK)
+		return res;
+
+	// ToDo: create hash-map from catalogMsg
+	fCatMap.clear();
+	
+}
+
+status_t
+DefaultCatalog::WriteToDisk(const char *path) const
+{
+	status_t res = B_OK;
+	BMessage archive;
+	CatMap::const_iterator iter;
+	for (iter = fCatMap.begin(); res==B_OK && iter!=fCatMap.end(); ++iter) {
+		res = archive.AddString( "c:key", iter->first.fKey.String())
+			|| archive.AddInt32( "c:hash", iter->first.fHashVal)
+			|| archive.AddString( "c:tstr", iter->second.String());
+	}
+
+	if (res == B_OK) {
+		if (path)
+			fPath = path;
+		BFile catalogFile;
+		res = catalogFile.SetTo(fPath.String(), 
+			B_WRITE_ONLY | B_CREATE_FILE | B_ERASE_FILE);
+	}
+	return res;
 }
 
 const char *
@@ -84,13 +170,25 @@ DefaultCatalog::GetString(uint32 id)
 	return "default-string-by-id";
 }
 
+status_t
+DefaultCatalog::SetString(const char *string, const char *translated, 
+	const char *context, const char *comment)
+{
+	return B_OK;
+}
+
+status_t
+DefaultCatalog::SetString(uint32 id, const char *translated)
+{
+	return B_OK;
+}
+
 
 
 BCatalogAddOn *
-DefaultCatalog::Instantiate(const char *signature, const char *language,
-	BCatalogAddOnInfo *info)
+DefaultCatalog::Instantiate(const char *signature, const char *language)
 {
-	DefaultCatalog *catalog = new DefaultCatalog(signature, language, info);
+	DefaultCatalog *catalog = new DefaultCatalog(signature, language);
 	if (catalog && catalog->InitCheck() != B_OK) {
 		delete catalog;
 		return NULL;
