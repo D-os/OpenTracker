fa0dc04e4195db2da0b07b9d02d01e091cef51cc
zooey <>
2004-01-12 22:55:40 +0000

    - changed around to use a testing class (such that the TR-macros work) - added loading and testing of an add-on which uses the locale-kit   itself (catalogTestAddOn). - added some more tests

--- locale/test/catalogTest.cpp
+++ locale/test/catalogTest.cpp
@@ -4,67 +4,150 @@
 */
 
 #include <stdio.h>
+#include <unistd.h>
 
 #include <Application.h>
-
 #include <Catalog.h>
 #include <DefaultCatalog.h>
+#include <Entry.h>
 #include <Locale.h>
+#include <Path.h>
+#include <Roster.h>
+
+class CatalogTest {
+	public:
+		void Run();
+		void Check();
+};
+
+
+#define catSig "x-vnd.Be.locale.catalogTest"
+#define catName catSig".catalog"
+
+void 
+CatalogTest::Run() {
+	printf("app...", be_locale);
+	status_t res;
+	BString s("string");
+	s << "\x01" << typeid(*this).name() << "\x01";
+	size_t hashVal = __stl_hash_string(s.String());
+	assert(be_locale != NULL);
+	system("mkdir -p ./locale/catalogs/"catSig);
+
+	// create an empty catalog of default type...
+	BCatalog cat1("Default", catSig, "German");
+	assert(cat1.InitCheck() == B_OK);
+	// ...and populate the catalog with some data:
+	DefaultCatalog *catalog = dynamic_cast<DefaultCatalog*>(cat1.fCatalog);
+	assert(catalog != NULL);
+	res = catalog->SetString("string", "Schnur", typeid(*this).name());
+	assert(res == B_OK);
+	res = catalog->SetString(hashVal, "Schnur_id");
+		// add a second entry for the same hash-value, but with different translation
+	assert(res == B_OK);
+	res = catalog->SetString("string", "String", "programming");
+	assert(res == B_OK);
+	res = catalog->SetString("string", "Textpuffer", "programming", "Deutsches Fachbuch");
+	assert(res == B_OK);
+	res = catalog->SetString("string", "Leine", typeid(*this).name(), "Deutsches Fachbuch");
+	assert(res == B_OK);
+	res = catalog->WriteToDisk("./locale/catalogs/"catSig"/german.catalog");
+	assert(res == B_OK);
+	// check if we are getting back the correct strings:
+	s = catalog->GetString("string", typeid(*this).name());
+	assert(s == "Schnur");
+	s = catalog->GetString(hashVal);
+	assert(s == "Schnur_id");
+	s = catalog->GetString("string", "programming");
+	assert(s == "String");
+	s = catalog->GetString("string", "programming", "Deutsches Fachbuch");
+	assert(s == "Textpuffer");
+	s = catalog->GetString("string", typeid(*this).name(), "Deutsches Fachbuch");
+	assert(s == "Leine");
+
+	printf("ok.\n");
+	Check();
+}
+
+
+void 
+CatalogTest::Check() {
+	status_t res;
+	printf("app-check...");
+	BString s("string");
+	s << "\x01" << typeid(*this).name() << "\x01";
+	size_t hashVal = __stl_hash_string(s.String());
+	// ok, we now try to re-load the catalog that has just been written:
+	//
+	// actually, the following code can be seen as an example of what an 
+	// app needs in order to translate strings:
+	BCatalog cat;
+	res = be_locale->GetAppCatalog(&cat);
+	assert(res == B_OK);
+	// fetch basic data:
+	int32 fingerprint;
+	res = cat.GetFingerprint(&fingerprint);
+	assert(res == B_OK);
+	BString lang;
+	res = cat.GetLanguage(&lang);
+	assert(res == B_OK);
+	BString sig;
+	res = cat.GetSignature(&sig);
+	assert(res == B_OK);
+	// now check strings:
+	s = TR("string");
+	assert(s == "Schnur");
+	s = TR_ID(hashVal);
+	assert(s == "Schnur_id");
+	s = TR_ALL("string", "programming", "");
+	assert(s == "String");
+	s = TR_ALL("string", "programming", "Deutsches Fachbuch");
+	assert(s == "Textpuffer");
+	s = TR_CMT("string", "Deutsches Fachbuch");
+	assert(s == "Leine");
+
+	// now check if trying to access same catalog by specifying its data works:
+	BCatalog cat2(sig.String(), lang.String(), fingerprint);
+	assert(cat2.InitCheck() == B_OK);
+	// now check if trying to access same catalog with wrong fingerprint fails:
+	BCatalog cat3(sig.String(), lang.String(), fingerprint*-1);
+	assert(cat3.InitCheck() == B_NO_INIT);
+	printf("ok.\n");
+}
+
 
 int
 main(int argc, char **argv)
 {
 	BApplication* testApp 
-		= new BApplication("application/x-vnd.otlocale.test-catalog");
-	BCatalog cat1("Test");
-	if (cat1.InitCheck() == B_OK) {
-		printf("translating %s in cat1 yields %s\n", "test", cat1.GetString( "test"));
-	}
-	BCatalog cat2("Test2");
-	if (cat2.InitCheck() == B_OK) {
-		printf("translating %s in cat2 yields %s\n", "test", cat2.GetString( "test"));
-	}
-	status_t res;
-	BString s;
-	size_t hashVal = __stl_hash_string("string\x01\x01");
-	DefaultCatalog catalog("TestCat", "german", "./TestCat.catalog", true);
-	if (catalog.InitCheck() == B_OK) {
-		res = catalog.SetString("string", "Schnur");
-		assert( res == B_OK);
-		res = catalog.SetString(hashVal, "Schnur_id");
-			// add a second entry for the same hash-value, but with different translation
-		assert( res == B_OK);
-		res = catalog.SetString("string", "String", "programming");
-		assert( res == B_OK);
-		res = catalog.SetString("string", "Textpuffer", "programming", "Deutsches Fachbuch");
-		assert( res == B_OK);
-		res = catalog.SetString("string", "Leine", "", "Deutsches Fachbuch");
-		assert( res == B_OK);
-		res = catalog.WriteToDisk();
-		assert( res == B_OK);
-		s = catalog.GetString("string");
-		assert( s == "Schnur");
-		s = catalog.GetString(hashVal);
-		assert( s == "Schnur_id");
-		s = catalog.GetString("string", "programming");
-		assert( s == "String");
-		s = catalog.GetString("string", "programming", "Deutsches Fachbuch");
-		assert( s == "Textpuffer");
-		s = catalog.GetString("string", "", "Deutsches Fachbuch");
-		assert( s == "Leine");
-	}
-	DefaultCatalog catalog2("TestCat", "german", "./TestCat.catalog", false);
-	if (catalog2.InitCheck() == B_OK) {
-		s = catalog2.GetString("string");
-		assert( s == "Schnur");
-		s = catalog2.GetString(hashVal);
-		assert( s == "Schnur_id");
-		s = catalog2.GetString("string", "programming");
-		assert( s == "String");
-		s = catalog2.GetString("string", "programming", "Deutsches Fachbuch");
-		assert( s == "Textpuffer");
-		s = catalog2.GetString("string", "", "Deutsches Fachbuch");
-		assert( s == "Leine");
-	}
+		= new BApplication("application/"catSig);
+	
+	// change to app-folder:
+	app_info appInfo;
+	be_app->GetAppInfo(&appInfo);
+	BEntry appEntry(&appInfo.ref);
+	BEntry appFolder;
+	appEntry.GetParent( &appFolder);
+	BPath appPath;
+	appFolder.GetPath( &appPath);
+	chdir( appPath.Path());
+
+	CatalogTest catTest;
+	catTest.Run();
+
+	char cwd[B_FILE_NAME_LENGTH];
+	getcwd(cwd, B_FILE_NAME_LENGTH);
+	BString addonName(cwd);
+	addonName << "/" "catalogTestAddOn";
+	image_id image = load_add_on(addonName.String());
+	assert(image >= B_OK);
+	void (*runAddonFunc)() = 0;
+	get_image_symbol(image, "run_test_add_on",
+		B_SYMBOL_TYPE_TEXT, (void **)&runAddonFunc);
+	assert(runAddonFunc);
+	runAddonFunc();
+
+	catTest.Check();
+
 	delete testApp;
 }
