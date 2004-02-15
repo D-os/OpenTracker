ed8b9cc73452da693bbbc482c0684b37c9f93a6b
bonefish <>
2004-02-15 21:31:42 +0000

    Added a skeleton for the first unit test.

---
+++ locale/test/number_format/NumberFormatTestAddOn.cpp
@@ -0,0 +1,15 @@
+#include <TestSuite.h>
+#include <TestSuiteAddon.h>
+
+#include "generic_number_format/GenericNumberFormatTests.h"
+
+// getTestSuite
+BTestSuite*
+getTestSuite()
+{
+	BTestSuite *suite = new BTestSuite("NumberFormat");
+
+	suite->addTest("BGenericNumberFormat", GenericNumberFormatTestSuite());
+	
+	return suite;
+}
